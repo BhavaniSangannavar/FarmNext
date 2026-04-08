1bdfae8bcf60aba8d4c73187c1d413537a914e4b
Bhavani Sangannavar <bhavanisangannavar403@gmail.com>
2026-04-08 18:56:16 +0530

    prepare for deployment

--- F2C/settings.py
+++ F2C/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$x$h0h_xk2$a9jqu-i^_9iqxlmbxa*qbstl1px1yyybww4x%=+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
